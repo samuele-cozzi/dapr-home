b3bb959184bbc4708dfc5b404adde0001c5baee2
samuele-cozzi <samuele.cozzi@gmail.com>
2022-09-04 10:34:34 +0000

    add service

--- src/apps/ui-ac-controller/src/app/ac-controller/ac-controller.component.ts
+++ src/apps/ui-ac-controller/src/app/ac-controller/ac-controller.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeApiService } from '../home-api.service';
 
 @Component({
   selector: 'app-ac-controller',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AcControllerComponent implements OnInit {
 
-  constructor() { }
+  constructor(private homeService: HomeApiService) { }
 
   ngOnInit(): void {
+    this.homeService.get();
   }
 
 }
--- src/apps/ui-ac-controller/src/app/home-api.service.ts
+++ src/apps/ui-ac-controller/src/app/home-api.service.ts
@@ -1,9 +1,36 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { DaprClient, HttpMethod } from "@dapr/dapr"; 
+//import { AcControllerCommand } from './ac-controller-command';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HomeApiService {
 
-  constructor() { }
+  private daprHost = "127.0.0.1"; 
+  private daprPort = "3500"; 
+  private serviceAppId = "home-api";
+  private serviceMethod = "home";
+
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private http: HttpClient) { 
+    
+  }
+
+  async get(){
+    var client = new DaprClient(this.daprHost, this.daprPort); 
+    var response = await client.invoker.invoke(this.serviceAppId , this.serviceMethod , HttpMethod.GET);
+    
+    console.log(response);
+    return response;
+  }
+
+  // set(command: AcControllerCommand): any {
+  //   const url = `${this.urlPrefix}/api/ac/control`
+  //   return this.http.post(url, command, this.httpOptions);
+  // }
 }
