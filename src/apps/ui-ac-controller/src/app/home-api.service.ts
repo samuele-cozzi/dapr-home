7ebfa555d84bda188b32df474f46a5a893a61f85
samuele-cozzi <samuele.cozzi@gmail.com>
2022-09-04 12:14:07 +0000

    correzioni bug

--- src/apps/ui-ac-controller/src/app/home-api.service.ts
+++ src/apps/ui-ac-controller/src/app/home-api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { DaprClient, HttpMethod } from "@dapr/dapr"; 
+//import { DaprClient, HttpMethod } from "@dapr/dapr"; 
 //import { AcControllerCommand } from './ac-controller-command';
 
 @Injectable({
@@ -8,25 +8,37 @@ import { DaprClient, HttpMethod } from "@dapr/dapr";
 })
 export class HomeApiService {
 
-  private daprHost = "127.0.0.1"; 
+  private daprHost = "http://127.0.0.1";
   private daprPort = "3500"; 
   private serviceAppId = "home-api";
   private serviceMethod = "home";
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 
+      'Content-Type': 'application/json',
+      'dapr-app-id': this.serviceAppId 
+    })
   };
 
   constructor(private http: HttpClient) { 
-    
   }
 
+  //constructor() { }
+
   async get(){
-    var client = new DaprClient(this.daprHost, this.daprPort); 
-    var response = await client.invoker.invoke(this.serviceAppId , this.serviceMethod , HttpMethod.GET);
-    
+
+    const url = `${this.daprHost}:${this.daprPort}/home`;
+    var response = this.http.get(url,this.httpOptions);
+
     console.log(response);
     return response;
+    
+    // var client = new DaprClient(this.daprHost, this.daprPort); 
+    // var response = await client.invoker.invoke(this.serviceAppId , this.serviceMethod , HttpMethod.GET);
+    
+    // console.log(response);
+    // return response;
+    return {};
   }
 
   // set(command: AcControllerCommand): any {
--- src/apps/ui-ac-controller/src/environments/environment.ts
+++ src/apps/ui-ac-controller/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  DAPR_HOST: "",
+  DAPR_HTTP_PORT: ""
 };
 
 /*
